d9b2cae647546e63b5747bca83ac0cb22e01ffbe
VytautasSim <vytautas.simanavicius@gmail.com>
2021-12-26 22:35:57 +0200

    JS and global css edit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // About section tab navigation
 const tabNavigationElements = document.querySelectorAll(".about .tab-navigation li");
-
+const tabContentElements = document.querySelectorAll(".about .tab-content");
 //console.log(tabNavigationElements)
 //about tage tab-navigation containeryje pasirenkame visus li
 //
@@ -15,7 +15,15 @@ function removeActiveClassesFromElements(elements) {
 
 for (let tabElements of tabNavigationElements) {
     tabElements.addEventListener("click", function() {
+        const target = this.dataset.target;
+        
         removeActiveClassesFromElements(tabNavigationElements);
         this.classList.add("active"); //uzdedame class "active" ant elemento kuri paspaudziame
-   });
+        
+        removeActiveClassesFromElements(tabContentElements)
+        document.querySelector('.tab-content[data-tab="'+target+'"]').classList.add('active'); 
+        //kiekviena karta selectins vis kita tab-content elementa pagal jo data-tab reiksme.
+        //t.y. ant kurio li tago mes paspaudziame
+        
+    });
 }
