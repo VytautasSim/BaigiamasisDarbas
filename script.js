50a36002a53cb0b1a5baf44ec8e5abec7f0e0381
VytautasSim <vytautas.simanavicius@gmail.com>
2022-01-05 22:28:19 +0200

    section header burger menu on mobile

--- script.js
+++ script.js
@@ -130,3 +130,19 @@ function setErrorFor(input, message) {
     //add error class
     form.Control.className = 'form-control error';
 }
+
+// Secition header javascript
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+
+}))
+    
